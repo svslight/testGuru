8cfa42e2fc923d232ff31d8854317423fc91e4bf
Svetlana Solodovnikova <svslight@yandex.ru>
2022-07-06 19:34:18 +0500

    Scope_Validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
-  has_many :tests
+  has_many :tests, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
+
+  default_scope { order(:name) }
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -5,7 +5,29 @@ class Test < ApplicationRecord
   has_many   :tests_users
   has_many   :users, through: :tests_users
 
-  def self.get_category_tests(name)
-    Test.joins(:category).where(categories: {name: name}).order('tests.title DESC').pluck('tests.title')
+  # default_scope { order(created_at: :desc) } 
+
+  # Может существовать только один Тест с данным названием и уровнем
+  # Наличие атрибута title
+  validates :title, presence: true, uniqueness: { scope: :level }
+
+  # Уровень Теста может быть только положительным целым числом
+  validates :level, numericality: { only_integer: true }
+
+  # Выбор Тестов по уровню сложности:
+  scope :easy, -> { where(level: 0..1).order(level: :asc) }
+  scope :mid, -> { where(level: 2..4).order(level: :asc) }
+  scope :hard, -> { where(level: 5..Float::INFINITY).order(level: :asc) }
+
+  # Получение Категорий,
+  # отсортированных по названию (tests.title desc) в порядке возрастания
+  scope :get_category_tests, ->(name) { joins(:category).where(categories: {name: name}) }
+
+  def self.order_by(name)
+    get_category_tests(name).order(title: :desc).pluck(:title)
   end
+
+  # def self.get_category_tests(name)
+  #   Test.joins(:category).where(categories: {name: name}).order('tests.title DESC').pluck('tests.title')
+  # end
 end
