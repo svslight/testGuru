b3646148fc56a81e50f089cada9bc03eda8fc322
Svetlana Solodovnikova <svslight@yandex.ru>
2022-07-18 18:21:19 +0500

    Develop9: Adding Index Tests (Title and Level)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_30_125710) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_18_130445) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_30_125710) do
     t.bigint "category_id", null: false
     t.bigint "user_id", null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
     t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
