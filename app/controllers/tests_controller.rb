3e2aebf739bc01f0afbd90464561b48b0b74fcaa
Svetlana Solodovnikova <svslight@yandex.ru>
2022-11-01 20:00:33 +0500

    Develop12: Localize Resources

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,7 +1,7 @@
 class TestsController < ApplicationController
 
   before_action :authenticate_user!
-  before_action :find_test, only: %i[ start ]
+  # before_action :find_test, only: %i[ start ]
   
   # GET    /tests
   def index
@@ -9,16 +9,19 @@ class TestsController < ApplicationController
   end
 
   def start
+    @test = Test.find(params[:id])
     current_user.tests.push(@test)          # тест привязывается к тек пользователю
     
-    # перенаправ на ресурс(test_passage-описать в модели User), отвечающий за прохождение конкретного теста
-    redirect_to current_user.test_passage(@test), notice: "Начался тест '#{@test.title}'"
+    # перенаправ на ресурс(test_passage-описать в модели User), 
+    # отвечающий за прохождение конкретного теста
+    # redirect_to current_user.test_passage(@test), notice: "Начался тест '#{@test.title}'"
+    redirect_to current_user.test_passage(@test), notice: t('.begin', title:@test.title )
   end
 
-  private 
+  # private 
 
-  def find_test
-    @test = Test.find(params[:id])
-  end
+  # if find_test
+  #  @test = Test.find(params[:id])
+  # end
   
 end
