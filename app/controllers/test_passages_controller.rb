b1b21061f7a10edfdd372b13e828273088487414
Svetlana Solodovnikova <svslight@yandex.ru>
2022-11-18 18:37:15 +0500

    Develop14: API External

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,9 +1,13 @@
 class TestPassagesController < ApplicationController
+  require "./app/services/result"
 
   before_action :authenticate_user!
-  before_action :set_test_passage, only: %i[show update result]
+  before_action :set_test_passage, only: %i[show update result gist]
 
   def show
+  end
+
+  def result    
   end
 
   def update
@@ -17,8 +21,17 @@ class TestPassagesController < ApplicationController
     end
   end
 
-  def result
-    
+  def gist
+
+    result = GistQuestionService.new(@test_passage.current_question).call
+
+    flash_options = if result.success?
+      { notice: t('.success') }
+    else
+      { alert: t('.failure') }
+    end
+
+    redirect_to @test_passage, flash_options
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :test_passages, only: %i[show update] do 
     member do
       get :result  # показать результат конкретного теста
+      post :gist   # новый маршрут post Для :gist, чтобы сохранить данный вопрос
     end
   end
 
@@ -28,5 +29,5 @@ Rails.application.routes.draw do
       end
     end
   end
-
+  
 end
