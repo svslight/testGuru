a0e3512e187a04c48917deaa7e88a9fedd7ee5d9
Svetlana Solodovnikova <svslight@yandex.ru>
2022-12-11 17:46:23 +0500

    Develop17: Deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # config.action_mailer.default_url_options = { host: 'svs-testguru.herokuapp.com' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.smtp_settings = {
+  #  address:              'smtp.gmail.com',
+  #  port:                 587,
+  #  domain:               "svs-testguru.herokuapp.com",
+  #  user_name:            ENV['SMTP_USERNAME'],
+  #  password:             ENV['SMTP_PASSWORD'],
+  #  authentication:       'plain',
+  #  enable_starttls_auto: true }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
